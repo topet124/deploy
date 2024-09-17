631ad2d309ad263bafabf298fd832a2f5a7735ea
Tope  Sule <57067649+topet124@users.noreply.github.com>
2024-09-17 14:48:24 +0300

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -33,9 +33,7 @@ export const login = async (req, res) => {
   res.cookie("token", token, {
   httpOnly: true,
   expires: new Date(Date.now() + oneDay),
-  secure: process.env.NODE_ENV === "production",
-  sameSite: "strict", // or 'lax' depending on your requirements
-  domain: process.env.NODE_ENV === "production" ? ".vercel.app" : "localhost"
+  secure: true,
 });
   res.status(StatusCodes.OK).json({ msg: "user logged in" });
 };
